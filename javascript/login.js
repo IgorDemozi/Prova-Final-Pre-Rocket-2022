117608e5ec14133b75bb07cdd0fb701f0d205153
Igor Demozi <igordemozi@gmail.com>
2022-08-02 21:57:32 -0300

    js do login iniciado

---
+++ javascript/login.js
@@ -0,0 +1,31 @@
+(function () {
+
+   $email = document.querySelector('.campo-email');
+   $senha = document.querySelector('.campo-senha');
+   $botao_entrar = document.querySelector('#login--botao');
+
+   var dados;
+
+   fetch('./data.json').then(response => {
+      return response.json();
+   }).then(body => { 
+      dados = body.data;
+   });
+
+   $botao_entrar.addEventListener('click', function login() {
+
+      if ($email.value == '' || $senha.value == ''){
+         alert('Informe E-Mail e senha');
+         return;
+      }
+
+      dados.login.forEach(item => {
+         
+         if ($email.value == item.email && $senha.value == item.password){
+            window.location.href = './index.html';
+            localStorage.setItem('atual-usuario', item.email);
+         }
+      });
+
+   });
+})()
